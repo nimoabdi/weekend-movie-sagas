c235c6f5f1b1aaff13d22aa7b3da8de81a24791a
Nimo Abdi <nimoabdi318@gmail.com>
2022-06-25 18:31:57 -0500

    details using params id

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,20 +1,32 @@
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {useParams} from 'react-router-dom';
+import {useEffect} from 'react';
+// import { useHistory } from 'react-router-dom';
 function MovieDetails() {
-    const history = useHistory();
-    const movie = useSelector(store => store.movie)
-    const genres = useSelector(store => store.genres)
+    const dispatch = useDispatch();
+    const params = useParams();
+    const fetchDetails = useSelector(store => store.movieDetails);
+    
+    useEffect(()=> {
+        dispatch({
+            type: 'GET_DETAILS',
+            payload: params.id
+        })
+    },[])
 
-    const backToHome = () => {
-        history.push('/')
-    }
+    // const backToHome = () => {
+    // }
 
     return (
-        <>
-      
-               
-            
-        </>
+        <div>
+            <h2>Movie view {params.id}</h2>
+            {fetchDetails.map(movie =><div key={item.id}>
+                    <h2>{movie.title}</h2>
+                    <img src={movie.poster}/>
+                    <p>{movie.description}</p>
+                </div>
+                )}
+        </div>
     )
 }
 export default MovieDetails;
