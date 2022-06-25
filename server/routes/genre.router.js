e6841c45daf24e91a040687fb175cd8416d23c0e
Nimo Abdi <nimoabdi318@gmail.com>
2022-06-25 18:33:01 -0500

    fixing routes

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,18 +4,6 @@ const pool = require('../modules/pool')
 
 router.get('/:id', (req, res) => {
   // Add query to get all genres
-  const queryText =
-  `SELECT movies.id, movies.title, genres.name FROM movies 
-       JOIN movies_genres ON movies.id = movies_genres.movie_id 
-        JOIN genres ON genres.id = movies_genres.genre_id 
-             WHERE movies.id= $1;`;
-  pool.query(queryText, [req.params.id])
-  .then((result)=> {
-    res.send(result.rows);
-  })          
-  .catch((error)=> {
-    console.log('error in GET genre DB', error)
-  }) 
   res.sendStatus(500)
 });
 
