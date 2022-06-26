dbbd908bc487742d3e9e2350d71583f3cba759c3
Nimo Abdi <nimoabdi318@gmail.com>
2022-06-26 14:33:02 -0500

    some mui

--- src/components/MovieItems/MovieItems.jsx
+++ src/components/MovieItems/MovieItems.jsx
@@ -35,26 +35,10 @@ function MovieItems() {
                 <form noValidate autoComplete='off' 
                 onSubmit={getMovie}>
             
-                     <TextField label='Title' variant='outlined' color='secondary' type='text' value={title} onChange={(evt)=> {setTitle(evt.target.value)}}/>
-                     <TextField label='Poster' variant='outlined' color='secondary' type='text' value={poster} onChange={(evt)=> {setPoster(evt.target.value)}}/>
-                     <TextField label='Description' variant='outlined' color='secondary' type='text' value={description} onChange={(evt)=> {setDescription(evt.target.value)}}/>
-        
-                     <select >
-                        <option value="1">Adventure</option>
-                        <option value="2">Animated</option>
-                        <option value="3">Biographical</option>
-                        <option value="4">Comedy</option>
-                        <option value="5">Disaster</option>
-                        <option value="6">Drama</option>
-                        <option value="7">Epic</option>
-                        <option value="8">Fantasy</option>
-                        <option value="9">Musical</option>
-                        <option value="10">Romantic</option>
-                        <option value="11">Science Fiction</option>
-                        <option value="12">Space Opera</option>
-                        <option value="13">Superhero</option>
-                     </select>
-                     <Button type='submit' color='secondary' variant='outlined'>Add a New Movie</Button>
+                     <TextField size='small' label='Title' variant='outlined' color='secondary' type='text' value={title} onChange={(evt)=> {setTitle(evt.target.value)}}/>
+                     <TextField size='small' label='Poster' variant='outlined' color='secondary' type='text' value={poster} onChange={(evt)=> {setPoster(evt.target.value)}}/>
+                     <TextField size='small' label='Description' variant='outlined' color='secondary' type='text' value={description} onChange={(evt)=> {setDescription(evt.target.value)}}/>
+                     <Button size='medium' type='submit' color='secondary' variant='outlined'>Add a New Movie</Button>
                  </form>
         </div>
         )
